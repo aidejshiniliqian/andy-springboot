390332e8601493bb6c6bf9cafec00c238be79e41
weiliukl <weiliukl@isoftstone>
2026-03-13 16:10:06 +0800

    GLM5 prompt： 当前项目是个空项目，现在需要从零开始搭建springboot，要求如下： 1. jdk使用21版本 2. springboot版本使用3.3.4 3. 集成mysql8.0 4. 集成mybatis，版本3.5.5 5. 需要使用hutool工具 6. 使用lombok代码简化工具 7. 使用fastjson作为json序列化工具，版本：2.0.54 集成完成后，开始第一个接口：/helloworld  POST  入参为name和say， 输出字符串：{name}, 你好，我想说：{say}

---
+++ src/main/java/com/andy/AndySpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.andy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndySpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AndySpringbootApplication.class, args);
+    }
+}
